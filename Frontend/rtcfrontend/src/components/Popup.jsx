7c45f92207a5ffe050af9a90bd1a7f76cf80cf1d
Avinash K <todisturbedlord@gmail.com>
2024-10-05 17:24:04 +0530

    Changed Popup Btn Text

--- Frontend/rtcfrontend/src/components/Popup.jsx
+++ Frontend/rtcfrontend/src/components/Popup.jsx
@@ -67,13 +67,13 @@ function Popup({ showPopup, hidePopup }) {
             <div className="bg-gray-50 px-4 py-2 sm:flex sm:flex-row-reverse sm:px-6">
               <button
                 type="button"
-                className="ml-2 mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                className="ml-2 mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-base font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
               >
                 Log In
               </button>
               <button
                 type="button"
-                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-base font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
               >
                 Cancel
               </button>
